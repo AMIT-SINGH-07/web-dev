1de9705484df42b0792601522fe7fbfc6ccbf2e9
AMIT SINGH <amit_239178@saitm.ac.in>
2024-04-15 12:18:22 +0530

    first commit

---
+++ DAY 4/script.js
@@ -0,0 +1,167 @@
+// function getdata(){
+//     {
+//         var a=12;
+//         let b=13;
+//         const c=14;
+//     }
+//     console.log(a);
+//     console.log(a);
+//     console.log(c);
+// }
+// getdata();
+// let a=12;
+
+// console.log(typeof(a));
+// let b="Hello";
+
+// console.log(typeof(b));
+// let c=true;
+
+// console.log(type(c));
+
+// let object={
+//     name: "AMIT SINGH",
+//     age:17,
+//     no:8502938797
+// };
+// console.log(object);
+
+// let ab=1;
+// ab
+
+// let A=3, B=4;
+// let sum = A+B;
+// let diff = A-B;
+// let prod =A*B;
+// let div = A/B;
+
+
+// console.log("a+b=", sum);
+// console.log("a-b=", diff);
+// console.log("a*b=", prod);
+// console.log("a/b=", div);
+
+// console.log(10==5);
+
+// let age = 19;
+
+// if (age > 18)
+// {
+//     console.log("you can vote");
+// }
+// else if (age < 18) {
+//     console.log("you cannot vote");
+// }
+
+// let a = [1,2,3," hello ",true,false];
+// console.log(a);
+// console.log(a[0])
+
+// a.push("AMIT SINGH")
+// console.log(a)
+
+//  a.prod
+//  a.pop()
+//  "AMIT SINGH"
+//  console.log(a)
+
+// let a =[1,2,3];
+// a.shift()
+// console.log(a);
+// a.unshift(121,24)
+// console.log(a);
+
+// let i=0;
+// while(i<10)
+// {
+//     console.log("hello"  + " "+ i)
+//     i=i + 1;
+// }
+
+// let i=0;
+// const ar = [1,2,3,4,5,6,7];
+// while (i < ar.length){
+//     i=i+1;
+//     console.log(ar[i]);
+// }
+
+// for(let i = 0; i < 10; ++i)
+// {
+//     console.log(i);
+// }
+
+// let array = [1,2,3,4];
+// for(let i = 0; i < array.length; i)
+// {
+//     console.log(array[i]);
+// }
+
+
+// const list = ['a','b','c','d','e','f'];
+// for (const value of list){
+//     console.log (value);
+// }
+
+// function add(a , b)
+// {
+//     let sum = a + b;
+//     console.log(sum);
+// }
+
+// add(10,12);
+// add(12,123);
+// add(234,131);
+// add(2904,4245);
+// add(2343,3);
+
+// function sub(a,b){
+//     let diff = a - b;
+//     return diff
+// }
+// let diff = sub (120,10);
+// console.log(diff);
+
+// ARITHMETIC CALCULATION
+
+function add (a,b)
+{
+    let add = a+b;
+    console.log(add);
+}
+add(12,20);
+add(50,50);
+
+function sub (a,b)
+{
+    let sub = a-b;
+    console.log(sub);
+}
+sub(34,24);
+sub(65,23);
+
+function mul (a,b)
+{
+    let mul = a*b;
+    console.log(mul);
+}
+mul(52,36);
+mul(40,60);
+
+function quo (a,b)
+{
+    let quo = a/b;
+    console.log(quo);
+}
+quo(12,20);
+quo(35,57);
+
+function re (a,b)
+{
+    let re = a%b;
+    console.log(re);
+}
+re(64,85);
+re(85,35);
+
+
+
